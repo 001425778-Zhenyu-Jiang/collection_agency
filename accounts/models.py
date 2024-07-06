b9ab83310f43fb416986e10aaa87fb386f5bd99a
Jenna Jiang <jenna.jiang@jennaji-ltmcye4.internal.salesforce.com>
2024-07-06 15:49:09 -0700

    update model

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Account(models.Model):
-    client_reference_no = models.CharField(max_length=100)
+    client_reference_no = models.CharField(max_length=255)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20)
     consumer_name = models.CharField(max_length=100)
-    consumer_address = models.TextField()
+    consumer_address = models.CharField(max_length=255)
     ssn = models.CharField(max_length=11)  # Adjust the length based on your format
 
     def __str__(self):
